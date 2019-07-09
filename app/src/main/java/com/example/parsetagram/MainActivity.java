3a3e4e6ac3539f9d22f452019368419f9347fcfc
acamacho4957 <acamacho4957@gmail.com>
2019-07-08 18:02:00 -0700

    Login page with styling, and Parse setup.

--- app/src/main/java/com/example/parsetagram/MainActivity.java
+++ app/src/main/java/com/example/parsetagram/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.parsetagram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.etUsername) EditText etUsername;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btLogin) Button btLogin;
+    @BindView(R.id.btSignup) Button btSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                Log.d("LoginActivity", String.format("Username: %s Password: %s ", username, password));
+                login(username, password);
+            }
+        });
+
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "User logged in successfully");
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.d("LoginActivity", "Login failed");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
