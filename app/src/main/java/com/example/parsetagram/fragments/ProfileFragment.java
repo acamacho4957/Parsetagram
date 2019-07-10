33693606449e4ef08eee93c796511313571d46c6
acamacho4957 <acamacho4957@gmail.com>
2019-07-10 15:58:02 -0700

    User can change their profile image

--- app/src/main/java/com/example/parsetagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parsetagram/fragments/ProfileFragment.java
@@ -1,32 +1,60 @@
 package com.example.parsetagram.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.parsetagram.MainActivity;
 import com.example.parsetagram.R;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
     private final String TAG = "ProfileFragment";
 
     @BindView(R.id.btLogout) Button btLogout;
+    @BindView(R.id.btChangeProfileImage) Button btChangeProfileImage;
+    @BindView(R.id.btRevert) Button btRevert;
+    @BindView(R.id.btAccept) Button btAccept;
     @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
     @BindView(R.id.tvUsername) TextView tvUsername;
+    @BindView(R.id.pbLoading) ProgressBar pbLoading;
+
+    public final static int CAPTURE_PROFILE_IMAGE_ACTIVITY_REQUEST_CODE = 1035;
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
+    private ParseUser currentUser;
+    private FragmentManager fragmentManager;
 
     public ProfileFragment() { }
 
@@ -40,7 +68,8 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         ButterKnife.bind(this, view);
-        ParseUser currentUser = ParseUser.getCurrentUser();
+        currentUser = ParseUser.getCurrentUser();
+        fragmentManager = getFragmentManager();
 
         tvUsername.setText(currentUser.getUsername().toString());
 
@@ -51,6 +80,31 @@ public class ProfileFragment extends Fragment {
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 startActivity(intent);
                 getActivity().finish();
+            }
+        });
+
+        btChangeProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
+        btAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (photoFile == null) {
+                    Toast.makeText(getContext(), "Missing image!", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveChange(photoFile);
+                }
+            }
+        });
+
+        btRevert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().detach(ProfileFragment.this).attach(ProfileFragment.this).commit();
             }
         });
 
@@ -65,5 +119,86 @@ public class ProfileFragment extends Fragment {
         } else {
             Log.d(TAG, "no profile image");
         }
+    }
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_PROFILE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_PROFILE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap rotatedImage = rotateBitmapOrientation(photoFile.getAbsolutePath());
+                ivProfileImage.setImageBitmap(rotatedImage);
+                btAccept.setVisibility(Button.VISIBLE);
+                btRevert.setVisibility(Button.VISIBLE);
+                btChangeProfileImage.setVisibility(Button.INVISIBLE);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void saveChange(File photoFile) {
+        pbLoading.setVisibility(ProgressBar.VISIBLE);
+        currentUser.put("profileImage", new ParseFile(photoFile));
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    pbLoading.setVisibility(ProgressBar.INVISIBLE);
+                    getFragmentManager().beginTransaction().detach(ProfileFragment.this).attach(ProfileFragment.this).commit();
+                    Log.d(TAG, "updated image successfully");
+                } else {
+                    Log.d(TAG, "error while saving");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Bitmap rotateBitmapOrientation(String photoFilePath) {
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoFilePath, bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap bm = BitmapFactory.decodeFile(photoFilePath, opts);
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(photoFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+        return rotatedBitmap;
     }
 }
